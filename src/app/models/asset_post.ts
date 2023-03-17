9c8fff46a7e9344168038e0b4f30827d41ac6208
Dong-Es <qufqlc164@gmail.com>
2023-03-17 13:57:01 +0900

    reset

---
+++ src/app/models/asset_post.ts
@@ -0,0 +1,34 @@
+import { ObjectLoader } from "../utils/objectloader";
+export const asset_id :string= "asset:prop:id";
+
+/* export class asset_post{
+  created ?: number;
+  properites ?:{
+    asset_id ?: string
+  }
+  id ?: string
+}
+*/
+export const asset = [
+  {
+    assets_id: "113-567-7645-6-123",
+    assets_title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    properites :{
+      asset_id : 123123123
+    }
+  },
+  {
+    assets_id: "543-456-3451-1-456",
+    assets_title: "search",
+    properites :{
+      asset_id : 456456456
+    }
+  },
+  {
+    assets_id: "543-456-3451-1-456",
+    assets_title: "search",
+    properites :{
+      asset_id : 789789789
+    }
+  }
+]
